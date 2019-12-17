d422bd585eef09bb1651fa1b0e60b929973dda2d
benyau <ben.yau@sicl.com.hk>
2019-12-17 16:42:42 +0800

    Change

--- src/App.js
+++ src/App.js
@@ -11,8 +11,15 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload Part 2.
         </p>
+		<a 
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer">
+			Learn React
+		</a>
       </div>
     );
   }
